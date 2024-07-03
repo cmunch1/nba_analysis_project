44f954c83ce1bf644f2d816603446eae69490d9c
Chris Munch <cmunch1@yahoo.com>
2024-07-03 07:31:20 -0500

    more comments

--- src/data_processing.py
+++ src/data_processing.py
@@ -2,6 +2,8 @@
 import pandas as pd
 
 
+
+
 def merge_scraped_dataframes(scraped_dataframes: list) -> pd.DataFrame:
     """
     Merges the scraped dataframes into a single dataframe by first standardizing the column names and then merging on GAME_ID and TEAM_ID.
@@ -13,17 +15,12 @@ def merge_scraped_dataframes(scraped_dataframes: list) -> pd.DataFrame:
         the merged dataframe with duplicated columns removed
 
     """
-       
-
-    # columns that are obvious duplicates that we will drop
-    drop_cols = ['Team', 'Match Up', 'Game Date', 'W/L', 'MIN',]
 
     # adjust column names so that they are all in standard format
     for df in scraped_dataframes:
         if 'TEAM' in df.columns:
             df = df.rename(columns={'TEAM':'Team', 'MATCH UP':'Match Up', 'GAME DATE':'Game Date'})
-        df.columns = df.columns.str.replace('\xa0', ' ') # weird text artifact in some column names
-        df = df.drop(columns=drop_cols) 
+        df.columns = df.columns.str.replace('\xa0', ' ') # weird web text artifact in some column names
 
     # merge all the dataframes, marking any columns that are duplicates with a suffix of '_dupe' so we can drop them later
     for i, df in enumerate(scraped_dataframes):
@@ -50,19 +47,20 @@ def process_nans(df: pd.DataFrame) -> pd.DataFrame:
 
     """
     
-    # drop rows where W/L is null (this represents a postponed or cancelled game and is very rare)
+    # drop rows where W/L is null (this represents a postponed or cancelled game and is very rare and even rarer that it is recorded in the data)
     df = df.dropna(subset=['W/L'])
 
-    # convert every field that has % in the field name to numeric
-    # these are calculated fields, and though very rarely, sometimes the divisor is 0
-    # and NBA.com puts a dash mark '-' in the calculated field 
-    # e.g. FT% (free throw percentage is calculated as FT/FTA (free throws made / free throws attempted)
-    # on the rare occasion that the team did not attempt a free throw, the field FT% (calculated 0/0) is marked as '-'
+    # Convert every field that has % in the field name to numeric.
+    # These are calculated fields, and though very rarely, sometimes the divisor is 0 and NBA.com puts a dash mark '-' in the calculated field.
     # 
-    # since these are so EXTREMELY rare, we are going to just convert to 75% (~ league average) for these cases so that we can make the field numeric
-    # had there been more of these cases, it MIGHT be worth investigating a better way to handle these cases or better way to impute the data
+    # e.g. FT% (free throw percentage is calculated as FT/FTA (free throws made / free throws attempted)
+    # On the rare occasion that the team did not attempt a free throw, the field FT% (calculated 0/0) is marked as '-'
+    # 
+    # Since these are so EXTREMELY rare (only 1 case in NBA history as of 2024), we are going to just convert to 75% (~ league average) 
+    # for these cases so that we can make the field numeric.
+    # Had there been more of these cases, it MIGHT be worth investigating a better way to handle these cases or better way to impute the data
     # or maybe use feature engineering to mediate the impact of these cases (maybe create a new field with a flag for these cases,
-    # or a new field that is FT% * FT to "weight" the FT% field by the number of free throws made, etc.)
+    # or a new field that is FT% * FT to "weight" the FT% field by the number of free throws made, etc.).
 
     for col in df.columns:
         if '%' in col:
@@ -72,7 +70,7 @@ def process_nans(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
+def rename_columns(df: pd.DataFrame) -> tuple[pd.DataFrame, dict]:
 
     """
     Renames the columns in the dataframe to make them more readable and to standardize the naming convention.
@@ -99,8 +97,7 @@ def rename_columns(df: pd.DataFrame) -> pd.DataFrame:
     # save a dictionary of the original column names to the new column names
     column_mapping = dict(zip(original_columns, df.columns.tolist()))
 
-    return df
-
+    return df, column_mapping
 
 
 def extract_new_columns(df: pd.DataFrame) -> pd.DataFrame:
@@ -117,7 +114,7 @@ def extract_new_columns(df: pd.DataFrame) -> pd.DataFrame:
         df (pd.DataFrame): the games dataframe
 
     Returns:
-        the cleaned dataframe
+        the dataframe with new columns extracted
 
     """
     
@@ -141,6 +138,7 @@ def extract_new_columns(df: pd.DataFrame) -> pd.DataFrame:
     df["is_playoff"] = (df["GAME_ID"].str[0].astype(int) > 2).astype(int)
 
     return df
+
 
 def reorder_columns(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -168,111 +166,8 @@ def reorder_columns(df: pd.DataFrame) -> pd.DataFrame:
     return df       
     
 
-def process_games(games: pd.DataFrame) -> pd.DataFrame:
-    """
-    Performs basic data cleaning on the games dataset.
-
-    Args:
-        games (pd.DataFrame): the raw games dataframe
-
-    Returns:
-        the cleaned games dataframe
-
-    """
-        
-   
-    # remove preseason games (GAME_ID begins with a 1)
-    games = games[games['GAME_ID'] > 20000000]
-
-    # flag postseason games (GAME_ID begins with >2)
-    games['PLAYOFF'] = (games['GAME_ID'] >= 30000000).astype('int8')
-
-    # remove duplicates (each GAME_ID should be unique)
-    games = games[~games.duplicated(subset=['GAME_ID'])]
-
-    # drop unnecessary fields
-    all_columns = games.columns.tolist()
-    drop_columns = ['GAME_STATUS_TEXT', 'TEAM_ID_home', 'TEAM_ID_away']
-    use_columns = [item for item in all_columns if item not in drop_columns]
-    games = games[use_columns]
-    
-    return games
 
 
-def process_ranking(ranking: pd.DataFrame) -> pd.DataFrame:
-    """
-    Performs basic data cleaning on the ranking dataset.
-    
-    Args:  
-        ranking (pd.DataFrame): the raw ranking dataframe
-
-    Returns:
-        the cleaned ranking dataframe
-      
-    """
-
-
-    # remove preseason rankings (SEASON_ID begins with 1)
-    ranking = ranking[ranking['SEASON_ID'] > 20000]
-
-    # convert home record and road record to numeric
-    ranking['HOME_W'] = ranking['HOME_RECORD'].apply(lambda x: x.split('-')[0]).astype('int')
-    ranking['HOME_L'] = ranking['HOME_RECORD'].apply(lambda x: x.split('-')[1]).astype('int')
-    ranking['HOME_W_PCT'] = ranking['HOME_W'] / ( ranking['HOME_W'] + ranking['HOME_L'] )
-
-    ranking['ROAD_W'] = ranking['ROAD_RECORD'].apply(lambda x: x.split('-')[0]).astype('int')
-    ranking['ROAD_L'] = ranking['ROAD_RECORD'].apply(lambda x: x.split('-')[1]).astype('int')
-    ranking['ROAD_W_PCT'] = ranking['ROAD_W'] / ( ranking['ROAD_W'] + ranking['ROAD_L'] )
-
-    # encode CONFERENCE as an integer (just using pandas - not importing sklearn for just one feature)
-    ranking['CONFERENCE'] = ranking['CONFERENCE'].apply(lambda x: 0 if x=='East' else 1 ).astype('int') 
-
-    # remove duplicates (there should only be one TEAM_ID per STANDINGSDATE)
-    ranking = ranking[~ranking.duplicated(subset=['TEAM_ID','STANDINGSDATE'])]
-
-    # drop unnecessary fields
-    drop_fields = ['SEASON_ID', 'LEAGUE_ID', 'RETURNTOPLAY', 'TEAM', 'HOME_RECORD', 'ROAD_RECORD']
-    ranking = ranking.drop(drop_fields,axis=1)
-
-    return ranking
-
-
-def process_games_details(details: pd.DataFrame) -> pd.DataFrame:
-    """
-    Performs basic data cleaning on the games_details dataset.
-
-    Args:
-        details (pd.DataFrame): the raw games_details dataframe
-    
-    Returns:
-        the cleaned games_details dataframe
-
-    """
-
-    
-    # convert MIN:SEC to float
-    df = details.loc[details['MIN'].str.contains(':',na=False)]
-    df['MIN_whole'] = df['MIN'].apply(lambda x: x.split(':')[0]).astype("int8")
-    df['MIN_seconds'] = df['MIN'].apply(lambda x: x.split(':')[1]).astype("int8")
-    df['MIN'] = df['MIN_whole'] + (df['MIN_seconds'] / 60)
-
-    details['MIN'].loc[details['MIN'].str.contains(':',na=False)] = df['MIN']
-    details['MIN'] = details['MIN'].astype("float16")
-
-    # convert negatives to positive
-    details['MIN'].loc[details['MIN'] < 0] = -(details['MIN'])
-
-    # update START_POSITION if did not play (MIN = NaN)
-    details['START_POSITION'].loc[details['MIN'].isna()] = 'NP'
-
-    # update START_POSITION if null
-    details['START_POSITION'] = details['START_POSITION'].fillna('NS')
-
-    # drop unnecessary fields
-    drop_fields = ['COMMENT', 'TEAM_ABBREVIATION', 'TEAM_CITY', 'PLAYER_NAME', 'NICKNAME'] 
-    details = details.drop(drop_fields,axis=1)
-    
-    return details
 
 
 def add_TARGET(df: pd.DataFrame) -> pd.DataFrame:
@@ -290,6 +185,7 @@ def add_TARGET(df: pd.DataFrame) -> pd.DataFrame:
     df['TARGET'] = df['HOME_TEAM_WINS']
     
     return df
+
 
 def split_train_test(df: pd.DataFrame) -> tuple[pd.DataFrame, pd.DataFrame]:
     """
