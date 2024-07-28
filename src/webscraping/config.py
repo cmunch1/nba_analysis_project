ad8ed646e186122dc314facca165470c016f42ca
Chris Munch <cmunch1@yahoo.com>
2024-07-28 11:54:27 -0500

    refactor july 28

--- src/webscraping/config.py
+++ src/webscraping/config.py
@@ -16,6 +16,10 @@ def load_config() -> dict:
 
     with open(config_path) as json_file:
         config = json.load(json_file)
+
+    # one of the game date headers has a unicode character in it "Game\xa0Date", which causes problems
+    config['game_date_header_variations'] = [header.encode().decode('unicode_escape') for header in config['game_date_header_variations']]
+
     
     return config
 
