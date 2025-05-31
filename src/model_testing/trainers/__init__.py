e1ff4982341d3258c1e8a7f5807d113ac00ff586
Chris Munch <cmunch1@yahoo.com>
2025-05-31 09:47:16 -0500

    pytorch

--- src/model_testing/trainers/__init__.py
+++ src/model_testing/trainers/__init__.py
@@ -3,11 +3,13 @@ from .xgboost_trainer import XGBoostTrainer
 from .lightgbm_trainer import LightGBMTrainer
 from .sklearn_trainer import SKLearnTrainer
 from .catboost_trainer import CatBoostTrainer
+from .pytorch_trainer import PyTorchTrainer 
 
 __all__ = [
     'BaseTrainer',
     'XGBoostTrainer',
     'LightGBMTrainer',
     'SKLearnTrainer',
-    'CatBoostTrainer'
-] 
+    'CatBoostTrainer',
+    'PyTorchTrainer' 
+]
